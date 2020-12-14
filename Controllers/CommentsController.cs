5f12bfe9fb5c88fbdedf058cab58d1dce4b7e079
Victor <victorcuru@gmail.com>
2020-12-14 14:48:27 -0300

    adicionando métodos put e delete

--- Controllers/CommentsController.cs
+++ Controllers/CommentsController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Data;
@@ -27,6 +28,49 @@ namespace Controllers
             await context.SaveChangesAsync();
             
             return Ok(model);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<ActionResult<List<Comment>>> Put(Guid id, [FromBody] Comment model, [FromServices] DataContext context)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if(id != model.Id)
+                return NotFound("Comentário não encontrado");
+            
+            try
+            {
+                context.Entry(model).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch(Exception)
+            {
+                return BadRequest(new { Message = "Não foi possível criar o comentário" });
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<ActionResult<List<Comment>>> Delete(Guid id, [FromServices] DataContext context)
+        {
+            var comment = await context.Comments.FirstOrDefaultAsync(x => x.Id == id);
+            
+            if(comment == null)
+                return NotFound("Comentário não encontrado");
+            
+            try
+            {
+                context.Comments.Remove(comment);
+                await context.SaveChangesAsync();
+                return Ok(new { Message = "Comentário deletado!" });
+            }
+            catch(Exception)
+            {
+                return BadRequest(new { Message = "Não foi apagar o comentário!" });
+            }
         }
     }
 }
